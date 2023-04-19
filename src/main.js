b0835c8ca6c4fd2f7ea015956326e593a6974131
Monique Lupe <moniquelupeandrade@gmail.com>
2023-04-19 16:38:45 -0300

    Criação das SAP

--- src/main.js
+++ src/main.js
@@ -2,8 +2,23 @@ import { Home } from './pages/home.js';
 import { Cadastro } from './pages/cadastro.js';
 import { Feed } from './pages/feed.js';
 
+const main = document.getElementById('root')
 const init = () => {
-  window.addEventListener("hashchange", () => console.log(window.location.hash))
+  window.addEventListener("hashchange", () => {
+    switch(window.location.hash){
+      case " ":
+        main.appendChild(Home());
+        break;
+      case "#cadastro":
+        main.appendChild(Cadastro());
+        break;
+      case "#feed":
+        main.appendChild(Feed());
+        break;
+      default:
+        main.appendChild(Home());
+    }
+  })
 }
 
 window.addEventListener("load", () => {
